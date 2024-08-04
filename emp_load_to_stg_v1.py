95814654fad479699eec98e87128cc5ea2952460
pradeepetl <59853089+pradeepetl@users.noreply.github.com>
2024-08-04 10:45:49 +0000

    added new sql file

--- emp_load_to_stg_v1.py
+++ emp_load_to_stg_v1.py
@@ -1,6 +1,13 @@
+# This file is part of Lecture,
+# Creating configuration file,
+#https://www.udemy.com/course/snowpark-data-engineering-with-snowflake/learn/lecture/36039772#overview
+
+#Collect rejects,
+#https://www.udemy.com/course/snowpark-data-engineering-with-snowflake/learn/lecture/36039468#overview
+
 import sys
 # Ignore the below command, if you are executing this code from Github codespaces
-sys.path.append('/Users/pradeep/Downloads/Udemy_course_videos/course_2_assignments/Snowpark_pipeline/')
+#sys.path.append('/Users/pradeep/Downloads/Udemy_course_videos/course_2_assignments/Snowpark_pipeline/')
 
 # Import generic code library.
 from generic_code import code_library
@@ -20,55 +27,8 @@ session = code_library.snowconnection(connection_parameter)
 copied_into_result, qid = code_library.copy_to_table(session,config_snow_copy,src_stg_schema.emp_stg_schema)
 rejects = code_library.collect_rejects(session, qid, config_snow_copy)
 
-employee_rejects
-
-
-
-
-
-
 print(copied_into_result)
 print(qid)
 
 copied_into_result_df = session.create_dataframe(copied_into_result)
 copied_into_result_df.show()
-
-
-
-
-
-
-
-f = open('/Users/pradeep/Downloads/Udemy_course_videos/course_2_assignments/Snowpark_etl/config/copy_to_snowflake.json', "r")
-config = json.loads(f.read())
-
-
-
-
-
-rejects.count()
-
-get_active_session()
-
-print(copied_into_result)
-
-print(qid)
-
-import pandas as pd
-from snowflake.connector.options import installed_pandas, pandas
-
-from snowflake.connector.options import installed_pandas
-
-from snowflake.connector.options import installed_pandas
-
-df = session.create_dataframe(pd.DataFrame([(1, 2, 3, 4)], columns=["a", "b", "c", "d"])).collect()
-
-print(df)
-
-df.show()
-
-df = session.table("DEMO_DB.PUBLIC.SNOWPARK_TEMP_TABLE_GL8Z56B6A4")
-#pip install "snowflake-connector-python[pandas]"
-
-if  installed_pandas:
-    print("hi")
