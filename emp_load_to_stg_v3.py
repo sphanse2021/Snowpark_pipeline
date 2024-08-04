2b6a28aabc55bec4f193eecd708c9ed5e12fc813
pradeepetl <59853089+pradeepetl@users.noreply.github.com>
2024-08-04 11:16:29 +0000

    code updated

--- emp_load_to_stg_v3.py
+++ emp_load_to_stg_v3.py
@@ -1,15 +1,25 @@
+# create or replace TABLE DEMO_DB.PUBLIC.EMP_DETAILS AVRO_CLS 
+# ( 
+# REGISTRATION VARCHAR(16777216),
+# USER_ID NUMBER(38,0),
+# FIRST NAME VARCHAR(16777216), 
+# LAST NAME VARCHAR(16777216), 
+# USER_EMAIL VARCHAR(16777216)
+# );
+
+
 import sys
-sys.path.append('/Users/pradeep/Downloads/Udemy_course_videos/course_2_assignments/Snowpark_pipeline/')
+#sys.path.append('/Users/pradeep/Downloads/Udemy_course_videos/course_2_assignments/Snowpark_pipeline/')
 from generic_code import code_library
 from schema import src_stg_schema
 from snowflake.snowpark.context import get_active_session
 import json
 
 ### Read from config file.
-config_snow_copy = open('/Users/pradeep/Downloads/Udemy_course_videos/course_2_assignments/Snowpark_pipeline/config/copy_to_snowstg_avro_v1.json', "r")
+config_snow_copy = open('./config/copy_to_snowstg_avro_v1.json', "r")
 config_snow_copy = json.loads(config_snow_copy.read())
 
-connection_parameter = open('/Users/pradeep/Downloads/Udemy_course_videos/course_2_assignments/Snowpark_pipeline/config/connection_details.json', "r")
+connection_parameter = open('./config/connection_details.json', "r")
 connection_parameter = json.loads(connection_parameter.read())
 
 
